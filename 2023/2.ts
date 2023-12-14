f7a4508504e2e94ae9454c492fb037809b6205f9
Conrad Shock <shockconrad@gmail.com>
2023-12-14 09:13:03 -0800

    2023 day 2

---
+++ 2023/2.ts
@@ -0,0 +1,61 @@
+import { log } from "console";
+import { processLines } from "./utilities.js";
+
+const lines = await processLines("2-input.txt");
+
+function pt1() {
+  const colorAllowance: { [key: string]: number } = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
+
+  let possibleGamesSum = 0;
+  let counter = 0;
+  lineLoop: for (const line of lines) {
+    ++counter;
+    const reveals = line.split(/(?::|;) /).slice(1);
+    revealLoop: for (const reveal of reveals) {
+      const colors = reveal.split(", ");
+      colorLoop: for (const color of colors) {
+        const [count, col] = color.split(" ");
+        if (Number(count) > colorAllowance[col]) {
+          continue lineLoop;
+        }
+      }
+    }
+    possibleGamesSum += counter;
+  }
+
+  log(possibleGamesSum);
+}
+
+pt1();
+
+function pt2() {
+  let sum = 0;
+
+  lineLoop: for (const line of lines) {
+    const minNeeded: { [key: string]: number } = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
+
+    const reveals = line.split(/(?::|;) /).slice(1);
+    revealLoop: for (const reveal of reveals) {
+      const colors = reveal.split(", ");
+      colorLoop: for (const color of colors) {
+        const [count, col] = color.split(" ");
+        minNeeded[col] = Math.max(minNeeded[col], Number(count));
+      }
+    }
+
+    const minNeededPow = minNeeded.red * minNeeded.green * minNeeded.blue;
+    sum += minNeededPow;
+  }
+
+  log(sum);
+}
+
+pt2();
